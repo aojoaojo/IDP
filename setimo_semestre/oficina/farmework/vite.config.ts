591e87365e9d9cae212e77c641ac530fdeb8e631
aojoaojo <aojoaojov@gmail.com>
2025-04-01 09:17:04 -0300

    ajustes deploy

--- setimo_semestre/oficina/farmework/vite.config.ts
+++ setimo_semestre/oficina/farmework/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/IDP/setimo_semestre/oficina/farmework/",
+  base: "/setimo_semestre/oficina/farmework/",
   plugins: [
     react(),
   ],
